833c47135a4e679ec9c4bbebec2f687a1bd7cfc2
Diana Wang <diana_wang@rp.edu.sg>
2023-09-28 14:13:17 +0800

    edit-child ball back

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // React-Router dependencies
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { ReviewProvider } from "./ReviewContext";
 // import in page components
 import ShowReviews from "./pages/ShowReviews";
@@ -9,14 +9,27 @@ import AddReview from "./pages/AddReview";
 // import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 function App() {
   return (
     <>
       <div className="container">
+
         <h1>RecipeBook App</h1>
         {/* All the routes must be defined as children of the BrowserRouter */}
         <ReviewProvider>
           <BrowserRouter>
+            <ul className="nav">
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/add"
+                >
+                  Add new review
+                </Link>
+              </li>
+            </ul>
+
             {/* All the routes must be in the <Routes> as children */}
             <Routes>
               <Route path="/" element={<ShowReviews />} />
--- src/pages/AddReview.jsx
+++ src/pages/AddReview.jsx
@@ -1,11 +1,30 @@
 import { useContext, useState } from "react";
 import { ReviewContext } from "../ReviewContext";
+import { useNavigate } from "react-router-dom";
+import ReviewForm from "../components/ReviewForm";
 
 export default function AddReview() {
+  const context = useContext(ReviewContext);
 
-    const context = useContext(ReviewContext);
+    // create the navigate hook
+    // we can use it to switch to a different route
+    const navigate = useNavigate();
 
-    return <>
-     <h1>Add new review</h1>
+  const processAddReview = (restaurant, review, rating) => {
+    context.addReview({
+        restaurant, review, rating
+    });
+    navigate('/');
+  }
+
+  return (
+    <>
+      <h1>Add new review</h1>
+      {/* we send to ReviewForm (the child component)
+      a function to call when the button in the form clicked */}
+      <ReviewForm buttonLabel="Add New"
+                  onSubmit={processAddReview}
+      />
     </>
+  );
 }
